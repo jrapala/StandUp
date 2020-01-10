357676a43d2127662c55767634ec865e1e4a089a
Juliette Rapala <juliette@eventric.com>
2020-01-10 15:10:38 -0600

    Add Prettier preferences and format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	semi: false,
+	tabWidth: 4,
+	useTabs: true,
+}
